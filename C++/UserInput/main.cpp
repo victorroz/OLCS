b670c19ad6af40ee11bc439afb257906ef9fe10f
Victor C. Rozario <victorroz@hotmail.com>
2020-11-05 22:42:04 +0600

    Bug fixed: String inputs

--- C++/UserInput/main.cpp
+++ C++/UserInput/main.cpp
@@ -4,27 +4,47 @@ using namespace std;
 
 int main()
 {
-    string carModel;
-    int carYear;
-    string carColor;
-    bool isFourWheelDrive;
+    /*
+    * Example 1
+    */
+    // string carModel;
+    // int carYear;
+    // string carColor;
+    // bool isFourWheelDrive;
 
-    cout << "Input Car Model: ";
-    cin >> carModel;
+    // cout << "Input Car Model: ";
+    // getline(cin, carModel);
 
-    cout << "Input Car Year: ";
-    cin >> carYear;
+    // cout << "Input Car Year: ";
+    // cin >> carYear;
 
-    cout << "Input Car Color: ";
-    cin >> carColor;
+    // cout << "Input Car Color: ";
+    // cin >> carColor;
 
-    cout << "Is the car four wheel drive? ";
-    cin >> isFourWheelDrive;
+    // cout << "Is the car four wheel drive? ";
+    // cin >> isFourWheelDrive;
 
-    cout << "Car Model: " << carModel << endl;
-    cout << "Car Year: " << carYear << endl;
-    cout << "Car Color: " << carColor << endl;
-    cout << "Is car four wheel drive? " << isFourWheelDrive << endl;
+    // cout << "Car Model: " << carModel << endl;
+    // cout << "Car Year: " << carYear << endl;
+    // cout << "Car Color: " << carColor << endl;
+    // cout << "Is car four wheel drive? " << isFourWheelDrive << endl;
+
+    /*
+    * Example 2
+    */
+
+    int noOfApples;
+    string customerName;
+
+    cout << "Enter number of apples: ";
+    cin >> noOfApples;
+    cin.ignore();
+
+    cout << "Enter customer number: ";
+    getline(cin, customerName);
+
+    cout << "Apple count: " << noOfApples << endl;
+    cout << "Customer name: " << customerName << endl;
 
     return 0;
 }
