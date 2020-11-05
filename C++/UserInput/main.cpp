4782a5064414b5bc1f55258986725921c78a33a7
Victor C. Rozario <victorroz@hotmail.com>
2020-11-05 18:45:29 +0600

    Added User Input Directory

---
+++ C++/UserInput/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string carModel;
+    int carYear;
+    string carColor;
+    bool isFourWheelDrive;
+
+    cout << "Input Car Model: ";
+    cin >> carModel;
+
+    cout << "Input Car Year: ";
+    cin >> carYear;
+
+    cout << "Input Car Color: ";
+    cin >> carColor;
+
+    cout << "Is the car four wheel drive? ";
+    cin >> isFourWheelDrive;
+
+    cout << "Car Model: " << carModel << endl;
+    cout << "Car Year: " << carYear << endl;
+    cout << "Car Color: " << carColor << endl;
+    cout << "Is car four wheel drive? " << isFourWheelDrive << endl;
+}
