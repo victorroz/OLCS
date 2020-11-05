c2a5775101d6c03802c81a51d3addff6418d1038
Victor C. Rozario <victorroz@hotmail.com>
2020-11-05 18:18:11 +0600

    Added C++ Directory

---
+++ C++/HelloWorld/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//
+int main()
+{
+     int x;
+     double y;
+     x = 10;
+     y = 3.5;
+     double z = x * y * y;
+     cout << "Result: " << z << endl;
+     return 100;
+}
---
+++ C++/Quiz/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "   *" << endl;
+    // cout << "  ***" << endl;
+    // cout << " *****" << endl;
+    // cout << "*******" << endl;
+
+    // int a = 1;
+    // int b = 2;
+
+    // int temp = a;
+    // a = b;
+    // b = temp;
+
+    string bookTitle = "sdfkjsdf";
+    string bookAuthor = "sdfgsdfsd";
+    float bookPrice = 19.99;
+    string bookISBN = "921-652-1024";
+
+    cout << "==========AVAILABLE BOOK INFO==========\n" << endl;
+    cout << "Title: " << bookTitle << endl;
+    cout << "Author: " << bookAuthor << endl;
+    cout << "Price: BDT " << bookPrice << endl;
+    cout << "ISBN Number: " << bookISBN << "\n" << endl;
+    cout << "=============END OF RESULT=============" << endl;
+
+    return 0;
+}
+
