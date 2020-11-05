d8cc2c7afce5ac2308a54d8d992e02dfc191b959
Victor C. Rozario <victorroz@hotmail.com>
2020-11-05 19:59:52 +0600

    Correction: return 0 added

--- C++/IfElse/main.cpp
+++ C++/IfElse/main.cpp
@@ -4,57 +4,59 @@ using namespace std;
 
 int main()
 {
-    // string carModel;
-    // int carYear;
-    // string carColor;
-    // bool isFourWheelDrive;
+    string carModel;
+    int carYear;
+    string carColor;
+    bool isFourWheelDrive;
 
-    // cout << "Input Car Model: ";
-    // cin >> carModel;
+    cout << "Input Car Model: ";
+    cin >> carModel;
 
-    // cout << "Input Car Year: ";
-    // cin >> carYear;
+    cout << "Input Car Year: ";
+    cin >> carYear;
 
-    // cout << "Input Car Color: ";
-    // cin >> carColor;
+    cout << "Input Car Color: ";
+    cin >> carColor;
 
-    // cout << "Is the car four wheel drive? ";
-    // cin >> isFourWheelDrive;
+    cout << "Is the car four wheel drive? ";
+    cin >> isFourWheelDrive;
 
-    // cout << "Car Model: " << carModel << endl;
-    // cout << "Car Year: " << carYear << endl;
-    // cout << "Car Color: " << carColor << endl;
-    // if(isFourWheelDrive)
-    // {
-    //     cout << "Is car four wheel drive? Yes" << endl;
-    // }
-    // else
-    // {
-    //     cout << "Is car four wheel drive? No" << endl;
-    // }
-
-    string animal;
-    
-    cout << "Enter animal: ";
-    cin >> animal;
-
-    if(animal == "monkey")
+    cout << "Car Model: " << carModel << endl;
+    cout << "Car Year: " << carYear << endl;
+    cout << "Car Color: " << carColor << endl;
+    if(isFourWheelDrive)
     {
-        cout << "Eat banana!!";
-    }
-    else if(animal == "tiger")
-    {
-        cout << "Roarrrrr!!";
-    }
-    else if(animal == "gorilla")
-    {
-        cout << "hahahehehuhu!!";
+        cout << "Is car four wheel drive? Yes" << endl;
     }
     else
     {
-        cout << "Useless human";
+        cout << "Is car four wheel drive? No" << endl;
     }
+
+    // string animal;
+    
+    // cout << "Enter animal: ";
+    // cin >> animal;
+
+    // if(animal == "monkey")
+    // {
+    //     cout << "Eat banana!!";
+    // }
+    // else if(animal == "tiger")
+    // {
+    //     cout << "Roarrrrr!!";
+    // }
+    // else if(animal == "gorilla")
+    // {
+    //     cout << "hahahehehuhu!!";
+    // }
+    // else
+    // {
+    //     cout << "Useless human";
+    // }
     
 
     // cout << "Is car four wheel drive? " << isFourWheelDrive << endl;
+
+    return 0;
 }
