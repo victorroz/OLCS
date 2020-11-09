c1f68e920d5bdf5f8f17e675e9e09f897b4afa3a
Victor C. Rozario <victorroz@hotmail.com>
2020-11-09 19:46:33 +0600

    Added IfElse Directory

---
+++ C++/ForLoop/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    for(int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
+
+    // int count = 0;
+
+
+    // Increment
+    // cout << "Pre-increment: " << ++count << endl;
+    // cout << "Post-increment: " << count++ << endl;
+
+    // cout << count;
+
+    // Decrement
+    // cout << "Pre-decrement: " << --count << endl;
+    // cout << "Post-decrement: " << count-- << endl;
+
+    // cout << count;
+
+    return 0;
+}
