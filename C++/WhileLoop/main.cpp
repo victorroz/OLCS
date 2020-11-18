e3b7a5f3acd2321874f34f13fb4ab47e5c5c5bcb
Victor C. Rozario <victorroz@hotmail.com>
2020-11-18 18:57:50 +0600

    Added WhileLoop Directory

---
+++ C++/WhileLoop/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Example 1
+    // int i = 2;
+    // while(i < 10)
+    // {
+    //     cout << i << "\t";
+    //     i++;
+    // }
+    
+
+    // Example 2
+    // int i = 1;
+    // do
+    // {
+    //     cout << i << "\t";
+    //     i++;
+    // }
+    // while(i < 8);
+
+    return 0;
+}
