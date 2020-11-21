04c031be1dddf7939d4fe0c01409c54fd3339ea2
Victor C. Rozario <victorroz@hotmail.com>
2020-11-21 18:58:38 +0600

    Added more on Functions

--- C++/Function/main.cpp
+++ C++/Function/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// Non-parameterized Functions
 void newFunc() 
 {
     cout << "newFunc() executed" << endl;
@@ -13,13 +14,55 @@ int addFunc()
     return x;
 }
 
+// Parameterized Functions
+void printName(string name)
+{
+    cout << "My name is: " << name;
+}
+
+// Default Parameters
+double recArea(double length=1, double width=1)
+{
+    return length * width;
+}
+
+void personalTraits(string firstName, int age, string hairColor="Black")
+{
+    cout << firstName << endl;
+    cout << age << endl;
+    cout << hairColor << endl;   
+}
+
+// Function Overloading
+double add(double a, double b)
+{
+    cout << "Double Parameter" << endl;
+    return a + b;
+}
+
+int add(int a, int b)
+{
+    cout << "Int Parameter" << endl;
+    return a + b;
+}
+
 int main()
 {
-    newFunc();
+    // newFunc();
     
-    int var = addFunc();
+    // int var = addFunc();
 
-    cout << var;
+    // cout << var;
+
+    // printName("Victor");
+
+    // cout << "The result is: " << add(1.1, 2.999);
+
+    // cout << "The result is: " << recArea(2);
+
+    // personalTraits("Victor", 24, "Brown");\
+
+    cout << add(1,2);
     
     return 0;
 }
