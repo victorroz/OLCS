c9b2b9bc4f4307492f82fe4b9d85360873796940
Victor C. Rozario <victorroz@hotmail.com>
2021-01-07 19:31:57 +0600

    Added WorkingWithMultipleFiles Directory

---
+++ C++/WorkingWithMultipleFiles/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "test.cpp"
+using namespace std;
+
+int main()
+{
+    test("Victor");
+
+    return 0;
+}
---
+++ C++/WorkingWithMultipleFiles/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void test(string name)
+{
+    cout << "Hello " << name;
+}
