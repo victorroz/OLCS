28c17faddf582b688276449e571d2b6f8e4c4549
Victor C. Rozario <victorroz@hotmail.com>
2021-02-12 18:52:33 +0600

    Added VariablesAndDataTypes Directory

---
+++ Java/VariablesAndDataTypes/Main.java
@@ -0,0 +1,21 @@
+// package Java.VariablesAndDataTypes;
+
+public class Main {
+    public static void main(String[] args) {
+        // Datatypes
+        // Integer: int - for storing whole numbers [size: 4 bytes]
+        // Float: float - for storing fractional numbers [size: 4 bytes]
+        // Double: double - for storing fractional numbers [size: 8 bytes]
+        // Character: char - for storing a single character [size: 2 bytes]
+        // Boolean: boolean - for storing either true or false [size: 1 bit]
+        // String: String - for storing a sequence of characters [size: varies depending on the string]
+
+        int x = 3;
+
+        String name = "John";
+        int age = 20;
+
+        System.out.println("My name is " + name + ". I am " + age + " years old.");
+
+    }
+}
