a59fe526c2fd80b86d0d1e303617cf57cbf0e15d
Victor C. Rozario <victorroz@hotmail.com>
2021-02-14 18:27:56 +0600

    Added IfElse Directory

---
+++ Java/IfElse/Main.java
@@ -0,0 +1,41 @@
+// package Java.IfElse;
+
+public class Main {
+    public static void main(String[] args) {
+        // Example 1
+        // int age = 17;
+
+        // if (age >= 18) {
+        //     System.out.println("You can drive");
+        // } 
+        // else {
+        //     System.out.println("Here take a candy!!");
+        // }
+
+        // Example 2
+        // String weather = "Sabab";
+
+        // if (weather == "Rainy") {
+        //     System.out.println("Take an umbrella.");
+        // }
+        // else if (weather == "Sunny") {
+        //     System.out.println("Put on sunscreen.");
+        // }
+        // else if (weather == "Snowy") {
+        //     System.out.println("Wear boots");
+        // }
+        // else {
+        //     System.out.println("You are monkee");
+        // }
+
+        // Example 3
+        // boolean allSystemOk = true;
+
+        // if (!allSystemOk) {
+        //     System.out.println("Ready for takeoff.");
+        // }
+        // else {
+        //     System.out.println("You are fired.");
+        // }
+    }
+}
