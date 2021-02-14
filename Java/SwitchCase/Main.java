5d88224fad6d188f52d844b59ec8c5693c3067b2
Victor C. Rozario <victorroz@hotmail.com>
2021-02-14 18:53:49 +0600

    Added SwitchCase

---
+++ Java/SwitchCase/Main.java
@@ -0,0 +1,24 @@
+// package Java.SwitchCase;
+
+public class Main {
+    public static void main(String[] args) {
+        char alphabet = 'g';
+
+        switch (alphabet) {
+            case 'a':
+                System.out.println("Alpha");
+                break;
+            case 'b':
+                System.out.println("Beta");
+                break;
+            case 'c':
+                System.out.println("China");
+                break;
+            case 'd':
+                System.out.println("Delta");
+                break;
+            default:
+                System.out.println("Useless");
+        }
+    }
+}
