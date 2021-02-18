86d1e392f0c9edc4549942b4b01448e944e7df92
Victor C. Rozario <victorroz@hotmail.com>
2021-02-18 18:52:33 +0600

    Added Array Directory

---
+++ Java/Array/Main.java
@@ -0,0 +1,18 @@
+// package Java.Array;
+
+public class Main {
+    public static void main(String[] args) {
+        // Array Declaration
+        String[] colors = {"Blue", "Red", "Black"};
+
+        // Array Initialization
+        // colors[0] = "Pink";
+        // colors[1] = "Blue";
+
+        // System.out.println(colors.length);
+
+        for(int i = 0; i < colors.length; i++) {
+            System.out.println("Index[" + i + "]: " + colors[i]);
+        }
+    }
+}
