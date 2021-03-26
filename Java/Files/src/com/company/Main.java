eade290e28ba043fe6d5d5e8f3fba6e2e1d7646e
Victor C. Rozario <victorroz@hotmail.com>
2021-03-26 12:23:15 +0600

    Updated Files Directory

--- Java/Files/src/com/company/Main.java
+++ Java/Files/src/com/company/Main.java
@@ -1,12 +1,14 @@
 package com.company;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
+        // Example 1
         try {
-            File file = new File("W:\\file.txt");
+            File file = new File("file.txt");
             if (file.createNewFile()) {
                 System.out.println("Created");
             } else {
@@ -14,7 +16,19 @@ public class Main {
             }
         } catch (IOException e) {
             System.out.println("Error occurred.");
-//            e.printStackTrace();
+            e.printStackTrace();
+        }
+
+        // Example 2
+        try {
+            FileWriter fileWriter = new FileWriter("file2.txt");
+            fileWriter.write("Hello from file2.\n");
+            fileWriter.write("Hello from file2 line 2.");
+            fileWriter.close();
+            System.out.println("FileWrite Successfull.");
+        } catch (IOException e) {
+            System.out.println("Error occurred.");
+            e.printStackTrace();
         }
     }
 }
